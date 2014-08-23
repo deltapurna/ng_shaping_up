8ca5f9bb58d8a6e44b9a4c1502ae45a532b14ad3
delta <d@qiscus.com>
2014-08-23 12:26:30 +0700

    controller with directive

--- app.js
+++ app.js
@@ -5,18 +5,6 @@
     this.products = gems;
   });
 
-  app.controller('PanelController', function(){
-    this.tab = 3;
-
-    this.selectTab = function(setTab){
-      this.tab = setTab;
-    };
-
-    this.isSelected = function(checkTab){
-      return this.tab === checkTab;
-    };
-  });
-
   app.controller('ReviewController', function(){
     this.review = {};
 
@@ -30,6 +18,25 @@
     return {
       restrict: 'E',
       templateUrl: 'product-title.html'
+    };
+  });
+
+  app.directive('productPanels', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'product-panels.html',
+      controller: function(){
+        this.tab = 1;
+
+        this.selectTab = function(setTab){
+          this.tab = setTab;
+        };
+
+        this.isSelected = function(checkTab){
+          return this.tab === checkTab;
+        };
+      },
+      controllerAs: 'panel'
     };
   });
 
